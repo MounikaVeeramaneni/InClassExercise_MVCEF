ea3752397a3775fead2e26d4490f47ddf9bb1647
Mounika Veeramaneni <mounikaveeramaneni@usf.edu>
2021-04-04 23:26:21 -0400

    Add project files.

---
+++ MVC_EF_Start/Controllers/OrdersController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_EF_Start.DataAccess;
+using MVC_EF_Start.Models;
+using System.Linq;
+
+namespace MVC_EF_Start.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ApplicationDbContext dbContext;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            Store MyStore = new Store
+            {
+                Name = "Walmart",
+                Email = "walmart@walmart.com",
+                Phone = 999999999,
+                Location = "Tampa"
+
+            };
+
+            Product MyProduct1 = new Product
+            {
+
+                Name = "StrawBerries",
+                Description = "StrawBerries",
+                Price = 15,
+                Seller = "Kirkland",
+                Store = MyStore
+
+            };
+            Product MyProduct2 = new Product
+            {
+                Name = "BlueBerries",
+                Description = "BlueBerries",
+                Price = 10,
+                Seller = "Kirkland",
+                Store = MyStore,             
+
+            };
+
+            Order MyOrder = new Order
+            {
+                OrderDate = System.DateTime.Now,
+                OrderMessage ="Mounika's Order",
+                OrderTotal = 100
+            };
+
+            Orderdetail MyOrderDetail1 = new Orderdetail
+            {
+               Quantity = 5,
+               Product = MyProduct1,
+               Order =MyOrder
+            };
+
+            Orderdetail MyOrderDetail2 = new Orderdetail
+            {
+                Quantity = 3,
+                Product = MyProduct2,
+                Order = MyOrder
+            };
+            dbContext.Stores.Add(MyStore);
+            dbContext.Products.Add(MyProduct1);
+            dbContext.Products.Add(MyProduct2);
+            dbContext.Orders.Add(MyOrder);
+            dbContext.Orderdetails.Add(MyOrderDetail1);
+            dbContext.Orderdetails.Add(MyOrderDetail2);
+            dbContext.Orderdetails.Where(o => o.Quantity == 2);
+            dbContext.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ MVC_EF_Start/Models/EF_Models.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_EF_Start.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public System.DateTime OrderDate { get; set; }
+        public string OrderMessage { get; set; }
+        public double OrderTotal { get; set; }
+
+        public virtual ICollection<Orderdetail> Orderdetails { get; set; }
+    }
+
+    public class Orderdetail
+    {
+        public int OrderdetailID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public int OrderID { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual Order Order { get; set; }
+    }
+    public class Product
+    {
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Seller { get; set; }
+        public int StoreID { get; set; }
+
+        public virtual Store Store { get; set; }
+        public virtual ICollection<Orderdetail> Orderdetails { get; set; }
+    }
+
+    public class Store
+    {
+        public int StoreID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
